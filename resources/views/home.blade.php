827fb4254bb381e37bdc2bce8fe85c3cc29bc273
佐竹初音 <htnnn17@gmail.com>
2022-02-02 21:05:54 +0900

    20220202 文言修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('ログイン成功') }}
                 </div>
             </div>
         </div>
